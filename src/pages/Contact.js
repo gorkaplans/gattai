f8bbd6923cc5ab15e29e856990edcc07f9b377a6
Gorka Plans <gorkaplans@gmail.com>
2023-04-28 12:09:53 +0200

    changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -10,7 +10,7 @@ const Contact = () => {
      <main className="py-10">
         <div className="container mx-auto px-6">
           <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">
-            CONTACT
+            CONTACT test git
           </h1>
           <p className="text-center text-gray-600 mb-6">
             Aquí encontrarás toda la información que necesitas sobre mi proyecto.
